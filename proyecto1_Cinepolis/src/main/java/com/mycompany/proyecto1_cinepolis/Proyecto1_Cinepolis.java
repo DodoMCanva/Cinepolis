8e4dc13d0ddf8b1bc62e0408d4b483ce0d1e27a0
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2024-09-24 11:13:34 +0100

    Subir

---
+++ proyecto1_Cinepolis/src/main/java/com/mycompany/proyecto1_cinepolis/Proyecto1_Cinepolis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_cinepolis;
+
+/**
+ *
+ * @author Valeria
+ */
+public class Proyecto1_Cinepolis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
