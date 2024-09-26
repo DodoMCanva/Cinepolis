aef9a949dda1b845d5bbc898fdcdbeae23bfa8f9
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2024-09-25 22:08:46 -0600

    Sucursal parte 1

---
+++ src/main/java/persistencia/SucursalDAO.java
@@ -0,0 +1,46 @@
+package persistencia;
+
+import dto.SucursalDTO;
+import entidad.SucursalEntidad;
+import java.util.ArrayList;
+import java.util.List;
+import utilerias.Tabla;
+
+public class SucursalDAO implements ISurcursalDAO{
+
+    @Override
+    public ArrayList<SucursalDTO> leer() throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void guardar(SucursalEntidad sucursal) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(String nombre) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<SucursalEntidad> buscarporDireccion(String dir, Tabla Filtro) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public SucursalEntidad buscarporNombre(String nombre) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<SucursalEntidad> buscarSucursal(Tabla filtro) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public IConexionBD getConexionBD() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
