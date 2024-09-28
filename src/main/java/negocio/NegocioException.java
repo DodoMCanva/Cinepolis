72047128f2a2cf4061d201ba594c54f3f5f3116f
superQ78 <167489927+superQ78@users.noreply.github.com>
2024-09-28 09:03:32 -0700

    modi

--- src/main/java/negocio/NegocioException.java
+++ src/main/java/negocio/NegocioException.java
@@ -3,14 +3,7 @@ package negocio;
 import persistencia.PersistenciaException;
 
 public class NegocioException extends Exception {
-
-    public NegocioException(String message) {
-        super(message);
+    public NegocioException(String message, Throwable cause) {
+        super(message, cause);
     }
-
-    NegocioException(String mesaje, PersistenciaException e) {
-        super(mesaje, e);
-    }
-
-
 }
